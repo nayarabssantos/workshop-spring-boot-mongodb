93cf151292128652e7442cebe4beeb662e0b2716
Nayara Santos <nayy.xd@hotmail.com>
2023-07-19 15:49:13 -0300

    Insercao de usuario com post

--- workshopmongo/src/main/java/com/nayarasantos/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/nayarasantos/workshopmongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.nayarasantos.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,5 +32,14 @@ public class UserResource {
 
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+
+        User obj = service.fromDTO(objDTO);
+       obj = service.insert(obj);
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+       return ResponseEntity.created(uri).build();
     }
 }
--- workshopmongo/src/main/java/com/nayarasantos/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/nayarasantos/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.nayarasantos.workshopmongo.services;
 
 import com.nayarasantos.workshopmongo.domain.User;
+import com.nayarasantos.workshopmongo.dto.UserDTO;
 import com.nayarasantos.workshopmongo.repository.UserRepository;
 import com.nayarasantos.workshopmongo.services.service.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
         Optional<User> user = repo.findById(id);
 
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
