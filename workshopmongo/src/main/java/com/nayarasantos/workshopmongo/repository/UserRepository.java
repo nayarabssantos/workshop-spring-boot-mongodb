e2c2fd80ee00ecb17bf137ff5bb4d0377d246115
Nayara Santos <nayy.xd@hotmail.com>
2023-07-19 14:29:13 -0300

    Conectando ao MongoDB com repository e service

---
+++ workshopmongo/src/main/java/com/nayarasantos/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nayarasantos.workshopmongo.repository;
+
+import com.nayarasantos.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
